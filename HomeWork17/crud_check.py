3906ab46ce80ee2b007ad9da4308f6e14142b9ec
JuliaLitovenko <julia.litovenko@gmail.com>
2022-10-17 16:46:45 +0300

    Update crud_check.py

--- HomeWork17/crud_check.py
+++ HomeWork17/crud_check.py
@@ -1,56 +1,110 @@
-"""
-API testing CRUD user
-"""
-
-import requests
+import pytest
+import time
 import json
-import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from data.links import Main_Page_URl
+from data.page_objects import MainPage, UserPage, ChangeUserPage, LoginPage, UsersCred
 
-# values
-headers = {
-    'Content-Type': 'application/json'
-}
-urls = ["https://www.aqa.science/users/"]
+with open(
+        "resources/data.json",
+        "r") as f:
+    secret_variables = json.load(f)
 
-
-def test_create_user_api(cred_file):
-    # Create user
-    payload = json.dumps({
-        "username": f"User_{random.randint(0, 100)}",
-        "email": f"{random.randint(0, 100)}@usr.com",
-        "groups": []
-    })
-    response = requests.request("POST", urls[0], auth=cred_file, data=payload, headers=headers).json()
-    with open('added_user.json', "w") as f:
-        json.dump(response, f)
-    assert response["url"] is not None, "User was not created"
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--ignore-certificate-errors')
 
 
-def test_read_user_api(check_user, user_data):
-    # find created user in users list
-    with open('users_list.json', 'r') as f:
-        data = json.load(f)
-    value = ""
-    for item in data:
-        if item.get("url", None) == user_data:
-            value += item.get("url")
-    assert value == user_data, "User not found"
+s = Service('C:/Users/yuliia.litovenko/PycharmProjects/Automation/venv/chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+
+# Login to admin web
+driver.get(secret_variables['access_url'])
+name_field = driver.find_element(By.ID, LoginPage.login_field_id)
+password_field = driver.find_element(By.XPATH, LoginPage.password_field_id)
+submit_button = driver.find_element(By.XPATH, LoginPage.submit_button_id)
+
+name_field.send_keys(secret_variables["login"])
+time.sleep(1)
+password_field.send_keys(secret_variables["password"])
+submit_button.click()
+element_fo_found = driver.find_element(By.XPATH,
+                                       MainPage.page_header_id)
+assert element_fo_found.text == "Django administration"
 
 
-def test_update_user_api(user_data, cred_file):
-    # update created user
-    payload = json.dumps({
-        "url": user_data,
-        "username": f"magomed_{random.randint(0, 100)}",
-        "email": f"{random.randint(0, 100)}@mgm.com",
-        "groups": []
-    })
-    response = requests.request("PATCH", user_data, auth=cred_file, headers=headers, data=payload)
-    assert response.status_code == 200, "User not updated"
+# Add user
 
 
-def test_delete_user_api(user_data, cred_file):
-    # delete created user and erase file added_user.json
-    response = requests.request("DELETE", user_data, auth=cred_file)
-    open('added_user.json', 'w').close()
-    assert response.status_code == 204, "User not deleted"
+driver.get(Main_Page_URl)
+driver.find_element(By.XPATH, MainPage.user_list_button).click()
+time.sleep(1)
+driver.find_element(By.XPATH, UserPage.add_user_button).click()
+time.sleep(1)
+driver.find_element(By.XPATH, UserPage.user_name_field).send_keys(UsersCred.main_user_name)
+time.sleep(1)
+driver.find_element(By.XPATH, UserPage.password_field).send_keys(UsersCred.password)
+time.sleep(1)
+driver.find_element(By.XPATH, UserPage.password_confirmation_field).send_keys(UsersCred.password)
+time.sleep(1)
+driver.find_element(By.XPATH, UserPage.save_button).click()
+    
+
+time.sleep(5)
+
+#Update user info
+
+driver.get(Main_Page_URl)
+driver.find_element(By.XPATH, MainPage.user_list_button).click()
+time.sleep(1)
+driver.find_element(By.XPATH, UserPage.search_field).send_keys(UsersCred.main_user_name)
+time.sleep(1)
+driver.find_element(By.XPATH, UserPage.search_button).click()
+time.sleep(1)
+driver.find_element(By.XPATH, UserPage.user_data).click()
+time.sleep(1)
+driver.find_element(By.XPATH, ChangeUserPage.user_name_field).clear()
+time.sleep(1)
+driver.find_element(By.XPATH, ChangeUserPage.user_name_field).send_keys(UsersCred.changed_user_name)
+time.sleep(1)
+driver.find_element(By.XPATH, ChangeUserPage.save_button).click()
+
+
+time.sleep(5)
+
+# Delete user
+driver.get(Main_Page_URl)
+driver.find_element(By.XPATH, MainPage.user_list_button).click()
+time.sleep(1)
+driver.find_element(By.XPATH, UserPage.search_field).send_keys(UsersCred.changed_user_name)
+time.sleep(1)
+driver.find_element(By.XPATH, UserPage.search_button).click()
+time.sleep(1)
+driver.find_element(By.XPATH, UserPage.user_data).click()
+time.sleep(1)
+driver.find_element(By.XPATH, ChangeUserPage.delete_button).click()
+time.sleep(1)
+driver.find_element(By.XPATH, ChangeUserPage.submit_delete_button).click()
+
+
+#Get all users
+
+driver.get(Main_Page_URl)
+driver.find_element(By.XPATH, MainPage.user_list_button).click()
+time.sleep(1)
+users_list_elem = driver.find_elements(By.XPATH, UserPage.user_data)
+users_list = []
+for i in users_list_elem:
+    users_list.append(i.text)
+
+
+driver.close()
+
+
+
+
+
+
+
